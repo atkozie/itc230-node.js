6e895470f8aad2943df36beab4409a89aa6cd6f1
atkozie <atkozie@gmail.com>
2017-04-09 20:40:08 -0700

    IP address shot, fixed index.js file
    adds screenshot of the IP address to use to access port 3000 on localhost, replaces non-working code from class with simplified working code from modules. Works for assignment from wk 1.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+/* ------modified code form class, not working correctly...
+
 var http = require("http"), fs = require("fs");
-
-
 
 http.createServer(function(req,res) 
 {
@@ -9,8 +9,7 @@ http.createServer(function(req,res)
   switch(path) {
     case '/':
       res.writeHead(200, {'Content-Type': 'text/html'});
-          fs.readFile(__dirname + 'home.html', function(err, data){
-              
+          fs.readFile(__dirname + 'home.html', function(err, data){      
           });
       res.end(data);
       break;
@@ -24,4 +23,24 @@ http.createServer(function(req,res)
       break;
                 }
     
+}).listen(process.env.PORT || 3000);
+*/
+
+var http = require("http"); 
+http.createServer(function(req,res) {
+  var path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('This is the home page');
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('This is the about page');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+      break;
+    }
 }).listen(process.env.PORT || 3000);
