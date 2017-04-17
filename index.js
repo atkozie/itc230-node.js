8b99bf8f881c718204e162e00b4c70d74657741c
atkozie <atkozie@gmail.com>
2017-04-16 21:27:20 -0700

    Week 2 - Let's Get Modular
    Adds working modular exporting book.js file in /lib folder, that communicates with the index.js file

--- index.js
+++ index.js
@@ -1,57 +1,54 @@
-/* ------modified code form class, not working correctly...
+'use strict'
+var http = require("http"), fs = require("fs"), qs = require("querystring"); //we need the querystring in the URL. fs and qs are variable we create here
+var book = require('./lib/book.js'); //and we need the array from the other file
 
-var http = require("http"), fs = require("fs");
 
-http.createServer(function(req,res) 
-{
-    
-  var path = req.url.toLowerCase();
-  switch(path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/html'});
-          fs.readFile(__dirname + 'home.html', function(err, data){      
-          });
-      res.end(data);
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
-                }
-    
-}).listen(process.env.PORT || 3000);
-*/
+function serveStatic(res, path, contentType, responseCode){
+    if(!responseCode) responseCode = 200; //if successful and no error codes
+    fs.readFile(__dirname + path, function(err, data){
+        if(err){ //if there's an error
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('500 Server Error'); //tell the user
+        }
+        else { //if no errors
+            res.writeHead(responseCode, {'Content-Type': contentType});
+            res.end(data); //proceed to write the content
+        }
+    });
+}
 
-var http = require("http"), fs = require("fs"), qs = require('querystring'), books = require('demowk2'); //fs and qs are variables created here
-http.createServer(function(req,res) {
-    let url =  req.url.split("?"); //create a variable called url, splits the url around the question mark. Becomes an array with two items in it, separated by the ?
-    var params = qs.parse(url[1]); //parse the querystring into the piece you need, in this case the 
-    console.log(params);
-    var path = url[0].toLowerCase();
+
+http.createServer((req,res) => {
+    let url = req.url.split("?"); //create a variable called url, splits the url around the question mark. Becomes an array with two items in it, separated by the ?
+    let params = qs.parse(url[1]); //parse the querystring into the piece you need, in this case the 
+    console.log(params); 
+    let path = url[0].toLowerCase(); //fix URL to all lowercase
     switch(path) {
-        case '/':
+            
+        case '/': //if nothing after slash, it's the homepage
           res.writeHead(200, {'Content-Type': 'text/plain'});
           res.end('This is the home page');
           break;
-        case '/about':
+            
+        case '/about': //about page, etc...
           res.writeHead(200, {'Content-Type': 'text/plain'});
           res.end('This is the about page');
-          break;
-        case '/search':
-          res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.end('Welcome to search');
-          break;
+          break; 
+            
+        case '/get':
+          let found = book.get(params.title); //get the book's title
+          res.writeHead(200, {'Content-Type': 'text/plain'}); //gonna display the title in plain text
+          res.end('Results for ' + params.title + "\n" + JSON.stringify(found)); //display the info exported
+          break;   
+            
         case '/delete':
           res.writeHead(200, {'Content-Type': 'text/plain'});
-          res.end('Delete path');
-          break;
+          res.end('Entry for the title ' + params.title + ' has been deleted!');
+          break;  
+            
         default:
           res.writeHead(404, {'Content-Type': 'text/plain'});
-          res.end('Not found');
+          res.end('Sorry, page not found!');
           break;
     }
 }).listen(process.env.PORT || 3000); //view this on localhost at http://127.0.0.1:3000/
